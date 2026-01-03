15f5d15ce8ed01ec3fc9f4d44099107ee4db89d4
Shus13 <sushitkarki234@gmail.com>
2026-01-03 17:27:55 +0545

    postman API thing

--- app.js
+++ app.js
@@ -4,18 +4,27 @@
     // res.send("<h1>Hello, I'm Home Page</h1>")
 
     res.json({
-        message : "I am Home Page"
+        message : "I am Home Page",
+        status : "200 OK"
     })
  })
 
  app.get("/contact", (req, res)=> {
-    res.send("Hello, I'm Contact Page")
+   //  res.send("Hello, I'm Contact Page")
+
+   res.json({
+      message  : "I'm Contact Page"
+   })
  })
 
  app.get("/about",(req,res) => {
-    res.send("Hello, I'm About Page")
+   //  res.send("Hello, I'm About Page")
+
+   res.json({
+      message : "I'm About Page"
+   })
  })
 
  app.listen(3000,(req,res)=>{
-    console.log("Modejs has started at port 3000")
+    console.log("NodeJs has started at port 3000")
  })
