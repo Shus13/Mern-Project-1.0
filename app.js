47df3290af1a98c5929f4b979f11642fd67ed313
Shus13 <sushitkarki234@gmail.com>
2026-01-09 22:53:54 +0545

    Create API

--- app.js
+++ app.js
@@ -1,50 +1,72 @@
- const express = require("express");
- const app = express();
- const { connectDatabase } = require("./database/database");
- app.use(express.json());
- connectDatabase()
+const express = require("express");
+const app = express();
+const { connectDatabase } = require("./database/database");
+const Blog = require("./model/blogModel");
+app.use(express.json());
+connectDatabase();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
+app.get("/", (req, res) => {
+  // res.send("<h1>Hello, I'm Home Page</h1>")
 
- app.get("/", (req,res)=>{
-    // res.send("<h1>Hello, I'm Home Page</h1>")
+  res.json({
+    message: "I am Home Page",
+    status: "200 OK",
+  });
+});
 
-    res.json({
-        message : "I am Home Page",
-        status : "200 OK"
-    })
- })
+app.get("/services", (req, res) => {
+  //  res.send("Hello, I'm Services Page")
+  res.json({
+    message: "I'm Services Page",
+  });
+});
 
- app.get("/services", (req, res)=> {
-   //  res.send("Hello, I'm Services Page")     
-   res.json({
-      message : "I'm Services Page"
-   })
- } )
+app.get("/contact", (req, res) => {
+  //  res.send("Hello, I'm Contact Page")
 
- app.get("/contact", (req, res)=> {
-   //  res.send("Hello, I'm Contact Page")
+  res.json({
+    message: "I'm Contact Page",
+  });
+});
 
-   res.json({
-      message  : "I'm Contact Page"
-   })
- })
+app.get("/about", (req, res) => {
+  //  res.send("Hello, I'm About Page")
 
- app.get("/about",(req,res) => {
-   //  res.send("Hello, I'm About Page")
+  res.json({
+    message: "I'm About Page",
+  });
+});
 
-   res.json({
-      message : "I'm About Page"
-   })
- })
+app.post("/login", (req, res) => {
+  console.log("Login Request Received");
+  res.json({
+    message: "Login Request Successful",
+  });
+  //  res.send("Hello, I'm Login Page")
+});
 
- app.post("/login",(req,res) => {
-   console.log("Login Request Received")
-   res.json({
-      message : "Login Request Successful"
-   })
-   //  res.send("Hello, I'm Login Page")  
- })
+//  Create Blog API
 
- app.listen(3000,(req,res)=>{
-    console.log("NodeJs has started at port 3000")
- })
+app.post("/createBlog", async (req, res) => {
+   const title = req.body.title
+   const subtitle = req.body.subtitle
+   const description = req.body.description
+
+  // Insert into Database
+  await Blog.create({
+    title: title,
+    subtitle: subtitle,
+    description: description
+  });
+
+  res.json({
+    status: 200,
+    message: "Blog created successfully",
+  });
+});
+
+app.listen(3000, (req, res) => {
+  console.log("NodeJs has started at port 3000");
+});
--- database/database.js
+++ database/database.js
@@ -2,10 +2,6 @@
 const mongoose = require("mongoose");
 
 exports.connectDatabase = async () => {
-   await mongoose.connect("mongodb+srv://sushitkarki_db_user:%40sushit%401@cluster0.iata84x.mongodb.net/?appName=Cluster0").then(() => {
-      console.log("Database Connected Successfully")
-   }).catch((error) => {
-      console.log("Database Connection Failed")
-      console.log(error)
-   });
+   await mongoose.connect("mongodb+srv://sushitkarki_db_user:%40sushit%401@cluster0.iata84x.mongodb.net/?appName=Cluster0")
+   console.log("Database Connected Successfully")
 }
